595cfaba0820904fcc45d1ec9e7e634ac97986f1
Hirosh Koshila <hiroshkoshila7@gmail.com>
2022-09-13 17:52:28 +0530

    backend main task

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { map } from 'rxjs';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private data = {
+    forever: {},
+    forever2: {
+      gitfollowers: 600,
+    },
+  };
+
+  constructor(private httpService: HttpService) {}
+
+  getUser(params) {
+    return this.httpService
+      .get(`http://registry.npmjs.org/${params.name}/latest`)
+      .pipe(
+        map((response) => response.data),
+        map((data) => ({
+          ...this.data[params.name],
+          name: data.name,
+          dependencies: data.dependencies,
+        })),
+      );
   }
 }
