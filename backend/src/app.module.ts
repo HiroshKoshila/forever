0b4a927c476104e6168c217747ed8996e47bbf3c
Hirosh Koshila <hiroshkoshila7@gmail.com>
2022-09-14 00:00:13 +0530

    error handling

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,20 @@
+import { HttpErrorFilter } from './errors/http-error.filter';
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ForeverModule } from './forever/forever.module';
+import { APP_FILTER } from '@nestjs/core';
 
 @Module({
   imports: [HttpModule, ForeverModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpErrorFilter,
+    },
+  ],
 })
 export class AppModule {}
