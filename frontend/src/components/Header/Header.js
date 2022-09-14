64595d3a74d52d2d65794f6984c432ff45f3dd4b
Hirosh Koshila <hiroshkoshila7@gmail.com>
2022-09-14 13:22:29 +0530

    started frontend development

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Header() {
+
+    return (
+        <div>
+            <div className="row">
+                <div className="col-md-12">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ frontend/src/components/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function HomePage() {
+
+    return (
+        <div align="center" id="loginback">
+            <div className="card-form">
+                <h1>This is Home page</h1>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
